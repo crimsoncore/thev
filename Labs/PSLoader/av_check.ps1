4895ff0d6b935c7830986a0e3021f6054ea42547
luks_mbp <luks@crimsoncore.be>
2025-06-09 11:06:27 +0200

    labs

--- Labs/PSLoader/av_check.ps1
+++
@@ -1,7 +0,0 @@
-[PSCustomObject]@{
-    "Real-Time Protection"        = if ((Get-MpComputerStatus).RealTimeProtectionEnabled -eq $false) {"disabled"} else {"enabled"}
-    "Cloud-Delivered Protection"  = if ((Get-MpPreference).MAPSReporting -eq 0) { "disabled" } else { "enabled" }
-    "Automatic Sample Submission" = if ((Get-MpPreference).SubmitSamplesConsent -eq 2) { "disabled" } else { "enabled" }
-    "Periodic File Scanning"      = if ((Get-MpPreference).DisableScanningNetworkFiles -eq $true) {"disabled"} else {"enabled"}
-    "AMSI SCript scanning"        = if ((Get-MpPreference).DisableScriptScanning -eq $true) {"disabled"} else {"enabled"}
-} | Format-Table -AutoSize
